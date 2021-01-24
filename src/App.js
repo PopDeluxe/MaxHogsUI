539f48cbf769563acae24254f534e5bd270397b2
Spencer Haney <spencer.haney@macys.com>
2021-01-24 16:44:48 -0500

    add semantic UI

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
 
     useEffect(() => {
         //fetch('http://10.0.0.112:8080/api/v1/maxhogs/merged')
-        fetch('https://maxhogs.herokuapp.com/api/v3/maxhogs/merged/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijc5MzlhMmQwLTU1OTEtNGMxYy1hZjhjLWQ3MGRhODliYzU2ZSIsImlhdCI6MTYxMTUwNzQ4Mywic3ViIjoiZGV2ZWxvcGVyLzc0ODY0MGQxLTE0ZDktZGE4MS0wMjBjLWEwMGU2MGI2YzdjZSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI1Mi4yMDIuMjE5LjE0Il0sInR5cGUiOiJjbGllbnQifV19.2ICAXzn8T8tF8LGKAFPipFaLJIrURxY0-s9xu6rVGMshn6x7o_Ht4lfSPYLAwazz1Jb73j3Oych0oIZyQ6G46Q')
+        fetch('https://maxhogs.herokuapp.com/api/v3/maxhogs/merged/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjIwNDAwYTliLTFkNzktNDhlMS1iYjY0LTkyMWNjNDc4MWU0YiIsImlhdCI6MTYxMTUyNDY0MSwic3ViIjoiZGV2ZWxvcGVyLzc0ODY0MGQxLTE0ZDktZGE4MS0wMjBjLWEwMGU2MGI2YzdjZSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI1NC4yMTEuMTkuNjgiLCI1Mi4yMDIuMjE5LjE0Il0sInR5cGUiOiJjbGllbnQifV19.3WMiP8bphKBSRQwyJsX1wukU9ya5c2iTAjZ9aZhJ9nHU2nYpYf9eIUnTieeqwkgvjUbmiN8X6tvhV1RhMdRm_A')
         .then(result => result.json())
         .then(rowData => setRowData(rowData))
     }, []);
