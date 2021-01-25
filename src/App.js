fe68e65db97dba4e8a62605f9e725897b17b1088
Spencer Haney <spencer.haney@macys.com>
2021-01-25 17:40:54 -0500

    add semantic UI

--- src/App.js
+++ src/App.js
@@ -1,205 +1,28 @@
+import React from 'react'
+import { Tab, Grid, Icon } from 'semantic-ui-react'
 
-import React, { useState, useEffect } from 'react';
-
-import { Header, Image, Table, Card, Statistic, Label, Grid, TableBody, Icon, TableRow, Tab} from 'semantic-ui-react';
-
-
-
-
-
-const App = () => {
-    const [gridApi, setGridApi] = useState(null);
-    const [gridColumnApi, setGridColumnApi] = useState(null);
-
-
-   const [rowData, setRowData] = useState([]);
+import HogScoreMatrix from "./components/hogScoreMatrix";
+import HogScores from "./components/hogScores";
+import Welcome from "./components/welcome"
 
 
 
-
-
-    useEffect(() => {
-        //fetch('http://localhost:8080/api/v1/maxhogs/merged')
-        fetch('https://maxhogs.herokuapp.com/api/v2/maxhogs/merged')
-        .then(result => result.json())
-        .then(rowData => setRowData(rowData))
-    }, []);
-
-
-
-    return (
-
-        <div>
-
-
-
-
-        <Grid divided='vertically'>
-          <Grid.Row columns={4}>
-          <Grid.Column>
-
-          <Table basic='very' celled collapsing>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Donations</Table.HeaderCell>
-        <Table.HeaderCell>Donation Score</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
-
-    <Table.Body>
-      <Table.Row positive>
-        <Table.Cell textAlign='right'>
-          200+ <Icon name='thumbs up outline' />
-        </Table.Cell>
-        <Table.Cell>4</Table.Cell>
-      </Table.Row>
-      <Table.Row positive>
-        <Table.Cell textAlign='right'>
-          100 - 200 <Icon name='hand peace outline' />
-        </Table.Cell>
-        <Table.Cell>3</Table.Cell>
-      </Table.Row>
-      <Table.Row positive>
-        <Table.Cell textAlign='right'>
-          50 - 100 <Icon name='hand lizard outline' />
-        </Table.Cell>
-        <Table.Cell>2</Table.Cell>
-      </Table.Row>
-      <Table.Row warning>
-        <Table.Cell textAlign='right'>
-          {">"}0 - 50 <Icon name='thumbs down outline' />
-        </Table.Cell>
-        <Table.Cell>1</Table.Cell>
-      </Table.Row>
-      <Table.Row negative>
-        <Table.Cell textAlign='right'>
-          0 <Icon name='attention' />
-        </Table.Cell>
-        <Table.Cell>0</Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-
-
-  
-          </Grid.Column>
-
-          <Grid.Column>
-          <Table basic='very' celled collapsing>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>River Fame</Table.HeaderCell>
-        <Table.HeaderCell>Fame Score</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
-
-    <Table.Body>
-      <Table.Row positive>
-        <Table.Cell textAlign='right'>
-          10,000+ <Icon name='thumbs up outline' />
-        </Table.Cell>
-        <Table.Cell>4</Table.Cell>
-      </Table.Row>
-      <Table.Row positive>
-        <Table.Cell textAlign='right'>
-          5,000 - 10,00 <Icon name='hand peace outline' />
-        </Table.Cell>
-        <Table.Cell>3</Table.Cell>
-      </Table.Row>
-      <Table.Row positive>
-        <Table.Cell textAlign='right'>
-          1,000 - 5,000 <Icon name='hand lizard outline' />
-        </Table.Cell>
-        <Table.Cell>2</Table.Cell>
-      </Table.Row>
-      <Table.Row warning>
-        <Table.Cell textAlign='right'>
-          {">"}0 - 1,000 <Icon name='thumbs down outline' />
-        </Table.Cell>
-        <Table.Cell>1</Table.Cell>
-      </Table.Row>
-      <Table.Row negative>
-        <Table.Cell textAlign='right'>
-          0 <Icon name='attention' />
-        </Table.Cell>
-        <Table.Cell>0</Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-          </Grid.Column>
-
-          <Grid.Column>
-
-          <Table basic='very' celled collapsing>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Hog Score</Table.HeaderCell>
+const panes = [
+  { menuItem: 'What is this?', render: () => 
+        <Tab.Pane><Welcome /></Tab.Pane> }
         
-            </Table.Row>
-            </Table.Header>
+        
+        ,
+  { menuItem: 'Hog Scores', render: () => 
+        <Tab.Pane>
+            <Grid divided='vertically'>
+                <HogScoreMatrix/>
+                <HogScores/>
+            </Grid>
+        </Tab.Pane>},
+  
+]
 
-            <Table.Body>
-      <Table.Row positive>
-        <Table.Cell textAlign='right'>
-          Avg(Donation Score + Fame Score)
-        </Table.Cell>
+const MaxHogTabs = () => <Tab panes={panes} />
 
-      </Table.Row>
- 
-    </Table.Body>
-
-
-          </Table>
-          
-          </Grid.Column>
-              
-
-          <Grid.Column>
-          {/*
-          <Image src='https://api-assets.clashroyale.com/cards/300/ASSQJG_MoVq9e81HZzo4bynMnyLNpNJMfSLb3hqydOw.png'/>
-          */}
-              <Image src={`${process.env.PUBLIC_URL}/images/puzzled.png`} size='small'/>
-
-          </Grid.Column>
-    </Grid.Row>
-
-    <Grid.Row columns={1}>
-      
-      <Grid.Column>
-      <Card.Group centered>
-            {rowData.map( (row) => 
-
-              <Card >
-
-                <Card.Content>
-                  <Card.Header>{row.name}</Card.Header>
-                  <Card.Meta>{row.role}</Card.Meta>
-                  <Card.Description>
-                    donations:  {row.donations} {row.donations===0 ? <Icon name='attention' color='red'/> : ''}<br/>
-                    river fame:  {row.fame} {row.fame===0 ? <Icon name='attention' color='red'/> : ''}<br/>
-                    boat repairs:  {row.repairs}<br/>
-                    donations received:  {row.donationsReceived}<br/>
-                    donation score:  {row.donationScore}<br/>
-                    fame score:  {row.riverScore}<br/>
-                    unrounded hog score: {row.unroundedHogScore}<br/>
-                    <Statistic.Group>
-                      <Statistic>
-                      <Statistic.Label>Hog Score</Statistic.Label><Statistic.Value>{row.hogScore}</Statistic.Value>
-                      </Statistic>
-                      </Statistic.Group>
-                  </Card.Description>
-                </Card.Content>
-              </Card>
-              )}
-            </Card.Group>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-
-            
-
-        </div>
-    );
-};
-
-export default App;
+export default MaxHogTabs;
